112667bff0ab13aaca40ae3ef1dfd49b167578ca
Guilherme Celente <125476965+g-celente@users.noreply.github.com>
2024-03-20 10:52:09 -0300

    Update PAR OU IMPAR.py

--- LISTA DE CONDICIONAIS/PAR OU IMPAR.py
+++ LISTA DE CONDICIONAIS/PAR OU IMPAR.py
@@ -1,11 +1,9 @@
-p = []
-i = []
 while True:    
     numero = int(input("Digite um número: "))
     if numero % 2 == 0:
-        p.append(numero)
+       print(f"O Número é par")
     
     else:
-        i.append(numero)
-    print(f"Os números pares são: {p}")
-    print(f"Os números impares são: {i}")
+        print(f"O Número impar")
+        
+
