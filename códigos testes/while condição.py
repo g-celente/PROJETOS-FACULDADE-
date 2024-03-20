20c288d36bb4de14681eb4d85dd0e7782f0e65d0
guilherme_celente <125476965+g-celente@users.noreply.github.com>
2024-03-20 17:12:10 -0300

    TROCA DE VARIAVÉL

--- códigos testes/while condição.py
+++ códigos testes/while condição.py
@@ -5,7 +5,7 @@ media_sala = 0
 #enquanto o contador for menos que notas, executa o código 
 while cont <= notas: 
     nota =  float(input(f"Informe a nota: {cont} \n"))
-    soma_notas += notas #acumula as notas e faz com que todas que sejam pedidas, somem juntas
+    soma_notas += nota #acumula as notas e faz com que todas que sejam pedidas, somem juntas
     cont += 1
 media_sala = soma_notas/notas 
 print(f" A média da sala é: {media_sala}") 
