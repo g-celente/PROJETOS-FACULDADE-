4c214f877531e17e818e83a5c28175dd357c5528
guilherme_celente <125476965+g-celente@users.noreply.github.com>
2024-03-27 11:05:42 -0300

    comit

--- listaraciocínio.py/exercício n2.py
+++ listaraciocínio.py/exercício n2.py
@@ -2,7 +2,10 @@ soma = 0
 contador = 0
 while contador <= 10:
     nota = float(input("Digite uma nota: "))
-    soma += nota
-    contador += 1
+    if nota >10 or nota <0:
+        print ("Nota inválida, digite novamente...")
+    else: 
+        soma += nota
+        contador += 1
 media_notas = soma / contador  
 print(f"A media das somas é igual a: {media_notas:.2f}")
