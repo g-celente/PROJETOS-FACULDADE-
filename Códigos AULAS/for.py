ab03e9ee1670946def5ea4d2eaf4712f49c4db05
guilherme_celente <guilherme.celente@outlook.com>
2024-04-05 12:28:23 -0300

    Create for.py

---
+++ Códigos AULAS/for.py
@@ -0,0 +1,13 @@
+
+#for i in range (0,11):
+    #if i %2 !=0:
+        #print(f"{i} é impar!")
+
+    #else: 
+        #print(f"{i} é par!")
+
+nomes = ["Ana", "João", "Maria", "Paulo"] 
+
+for i in range(0,len(nomes)):
+    print(f" {i+1}. {nomes[i]}")
+    
