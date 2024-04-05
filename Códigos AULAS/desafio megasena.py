177bcd9af8921685cde5adaa943fbe660f0e69dc
guilherme_celente <guilherme.celente@outlook.com>
2024-04-05 11:17:00 -0300

    Update desafio megasena.py

--- Códigos AULAS/desafio megasena.py
+++ Códigos AULAS/desafio megasena.py
@@ -1,16 +1,28 @@
 #GERAR NÚMEROS ALEATÓRIOS COM PYTHON
 import random #biblioteca aleatória 
-p = []
+
 sorteio = [0,0,0,0,0,0]
 aposta = [0,0,0,0,0,0]
 i = 0
+acertos = []
 while i < len(sorteio):
     sorteio[i] =  random.randint(1,60)
     aposta[i] =  int(input(f"Digite o {i+1}° número: "))
+    
+    if aposta[i] > 60 or aposta[i] < 0:
+        print("Número repetido ou inválido, tente novamente....")
+        continue
     i +=1
-    if aposta == sorteio :
-        p.append(aposta)
 
-print (f"O valor sorteado é igual a: {sorteio}")
-print (f"A sua aposta deu: {aposta}")
-print(f"Os números que você acertou foram: {p}")
+    if aposta in sorteio: 
+        acertos.append(aposta[i])
+
+print(f"Os Números Sorteados foram: {sorteio}")
+print(f"Os Números Que você Apostou foram: {aposta}")
+print(f"No Total, você acertou {len(acertos)} números, que são: {acertos}")
+
+    
+
+
+
+    
